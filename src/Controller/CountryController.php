f3e587edd00468c1231419b12375b2f3f221dd82
Joshua Behrens <behrens@heptacom.de>
2020-03-21 17:00:29 +0100

    Add country controller
    #1

---
+++ src/Controller/CountryController.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\CountryRepository;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class CountryController implements ClassResourceInterface
+{
+    /**
+     * @var CountryRepository
+     */
+    private $countryRepository;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(CountryRepository $countryRepository, SerializerInterface $serializer)
+    {
+        $this->countryRepository = $countryRepository;
+        $this->serializer = $serializer;
+    }
+
+    public function cgetAction(): Response
+    {
+        $items = $this->countryRepository->findAll();
+        $itemsData = $this->serializer->serialize($items, 'json', [
+            'groups' => [
+                'default',
+            ]
+        ]);
+
+        return Response::create($itemsData);
+    }
+
+    public function getAction($id): Response
+    {
+        $item = $this->countryRepository->find($id);
+        $itemData = $this->serializer->serialize($item, 'json', [
+            'groups' => [
+                'default',
+                'country.translations',
+                'countryTranslation.locale',
+            ]
+        ]);
+
+        return Response::create($itemData);
+    }
+}
