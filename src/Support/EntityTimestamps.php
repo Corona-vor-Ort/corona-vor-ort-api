e9ffff2cb2288cb64c6416f68b9f06142ae1bac3
Joshua Behrens <behrens@heptacom.de>
2020-03-21 11:10:54 +0100

    Add locale database structure

---
+++ src/Support/EntityTimestamps.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\Support;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait EntityTimestamps
+{
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
