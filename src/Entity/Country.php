671a7b826abc6ab9c4f35665b2f0373ebc3b78c3
Joshua Behrens <behrens@heptacom.de>
2020-03-21 16:47:28 +0100

    Add country translations
    #1

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Support\EntityId;
 use App\Support\EntityTimestamps;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,6 +21,16 @@ class Country
      */
     private $iso;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\CountryTranslation", mappedBy="country", orphanRemoval=true)
+     */
+    private $translations;
+
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
+
     public function getIso(): ?string
     {
         return $this->iso;
@@ -27,6 +39,37 @@ class Country
     public function setIso(string $iso): self
     {
         $this->iso = $iso;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|CountryTranslation[]
+     */
+    public function getTranslations(): Collection
+    {
+        return $this->translations;
+    }
+
+    public function addTranslation(CountryTranslation $translation): self
+    {
+        if (!$this->translations->contains($translation)) {
+            $this->translations[] = $translation;
+            $translation->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTranslation(CountryTranslation $translation): self
+    {
+        if ($this->translations->contains($translation)) {
+            $this->translations->removeElement($translation);
+            // set the owning side to null (unless already changed)
+            if ($translation->getCountry() === $this) {
+                $translation->setCountry(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Locale.php
+++ src/Entity/Locale.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Support\EntityId;
 use App\Support\EntityTimestamps;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,6 +21,16 @@ class Locale
      */
     private $iso;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\CountryTranslation", mappedBy="locale", orphanRemoval=true)
+     */
+    private $countryTranslations;
+
+    public function __construct()
+    {
+        $this->countryTranslations = new ArrayCollection();
+    }
+
     public function getIso(): ?string
     {
         return $this->iso;
@@ -27,6 +39,37 @@ class Locale
     public function setIso(string $iso): self
     {
         $this->iso = $iso;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|CountryTranslation[]
+     */
+    public function getCountryTranslations(): Collection
+    {
+        return $this->countryTranslations;
+    }
+
+    public function addCountryTranslation(CountryTranslation $countryTranslation): self
+    {
+        if (!$this->countryTranslations->contains($countryTranslation)) {
+            $this->countryTranslations[] = $countryTranslation;
+            $countryTranslation->setLocale($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCountryTranslation(CountryTranslation $countryTranslation): self
+    {
+        if ($this->countryTranslations->contains($countryTranslation)) {
+            $this->countryTranslations->removeElement($countryTranslation);
+            // set the owning side to null (unless already changed)
+            if ($countryTranslation->getLocale() === $this) {
+                $countryTranslation->setLocale(null);
+            }
+        }
 
         return $this;
     }
