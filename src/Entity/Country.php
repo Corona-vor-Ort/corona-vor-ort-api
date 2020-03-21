fe839505c1a035cb05fa1674a1ce5bba3471eb68
Joshua Behrens <behrens@heptacom.de>
2020-03-21 16:24:45 +0100

    Add country entity
    #1

---
+++ src/Entity/Country.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Support\EntityId;
+use App\Support\EntityTimestamps;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CountryRepository")
+ */
+class Country
+{
+    use EntityId;
+    use EntityTimestamps;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $iso;
+
+    public function getIso(): ?string
+    {
+        return $this->iso;
+    }
+
+    public function setIso(string $iso): self
+    {
+        $this->iso = $iso;
+
+        return $this;
+    }
+}
